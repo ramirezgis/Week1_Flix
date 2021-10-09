2fc1207981315cce4418dbf5eb46616c44d5c053
ramirezgis <90123014+ramirezgis@users.noreply.github.com>
2021-10-09 01:33:08 -0700

    Week 1 - Movie App
    Made an app with movie title, poster, and movie summary.

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Week 1 App' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Week 1 App
+  pod "AlamofireImage"
+
+
+  target 'Week 1 AppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Week 1 AppUITests' do
+    # Pods for testing
+  end
+
+end
---
+++ Week 1 App/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  Week 1 App
+//
+//  Created by Giselle Ramirez on 10/8/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
