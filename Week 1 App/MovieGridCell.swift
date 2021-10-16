235004fc2649419dc2045e7ee8262da846a898fe
ramirezgis <90123014+ramirezgis@users.noreply.github.com>
2021-10-15 17:42:42 -0700

    Flix Updates

---
+++ Week 1 App/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Week 1 App
+//
+//  Created by Herman Ramirez on 10/15/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
